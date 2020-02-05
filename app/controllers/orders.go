549a7d7b75301b5e8358b79bce78ddeb553ddd87
Akinnurun Oluwafemi <femmy4lov@gmail.com>
2020-02-05 16:34:33 +0100

    updated index.html

---
+++ app/controllers/orders.go
@@ -0,0 +1,16 @@
+package controllers
+
+import "github.com/revel/revel"
+
+type Orders struct {
+	*revel.Controller
+}
+
+func (c Orders) Create() revel.Result {
+	return c.Render()
+}
+
+func (c Orders) GetPayment(orderId int) revel.Result {
+	 println(" The Order ID: ", orderId)
+	 return c.RenderTemplate("orders/payment.html")
+}
